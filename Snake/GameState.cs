2cbc8acd5881032405e4d7a4ff9a4937cfd9118a
VijayHYadav <vizzyadav1997@gmail.com>
2024-02-25 16:00:27 +0530

    snake helpers

--- Snake/GameState.cs
+++ Snake/GameState.cs
@@ -63,5 +63,20 @@ namespace Snake
             Position pos = empty[random.Next(empty.Count)];
             Grid[pos.Row, pos.Col] = GridValue.Food;
         }
+
+        public Position HeadPosition()
+        {
+            return snakePositions.First.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return snakePositions.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePositions()
+        {
+            return snakePositions;
+        }
     }
 }
